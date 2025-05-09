d45ff1a7c015371d924de1bcdc5c843a229461fa
Ayoubb <148903995+Ayoubb4@users.noreply.github.com>
2025-05-09 12:42:49 +0200

    Delete docs directory

--- docs/elestu-backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { registrarUsuario, loginUsuario } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/registro', registrarUsuario);
-router.post('/login', loginUsuario);
-
-module.exports = router;
