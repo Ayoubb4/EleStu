53cdbb54d8b2c306e07a49969ef676fe9db1fda8
Ayoub <ayoubmisbahfat.al19@emiliojimeno.edu.es>
2025-06-08 19:52:43 +0200

    SubidaStudiosOK7.7

--- elestu-be/src/data-source.ts
+++ elestu-be/src/data-source.ts
@@ -4,11 +4,11 @@ import { Service } from './services/service.entity';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT || '5432'),
-    username: process.env.DB_USER || 'EleStuAdmin',
-    password: process.env.DB_PASSWORD || '123456',
-    database: process.env.DB_NAME || 'EleStu',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [User, Service],
     migrations: ['src/migrations/**/*.ts'],
     synchronize: false,
